61141587074c8511999867ab301219647dcfbad4
Hank Liu <hanke.liu@gmail.com>
2020-05-05 15:56:30 -0400

    add method for instrument belongs to a band through musicians

--- collaborating.rb
+++ collaborating.rb
@@ -1,12 +1,14 @@
 # musician and instruments Collaborating Objects
-# band --< Musician >-- Instrument
+# band --< Musician -- Instrument
+
 
 class Band
-  attr_accessor :name
+  attr_accessor :name, :musicians
   @@all = []
 
   def initialize(name)
     @name = name
+    @musicians = []
     save
   end
 
@@ -18,10 +20,23 @@ class Band
     @@all
   end
 
+  #band has many instruments through musicians
+  def instruments
+    musicians.each do |m|
+      m.instrument.type
+    end
+  end
+
 end #end of class
 
 class Musician
-  attr_reader :name, :instrument
+  attr_reader :name, :instrument, :band
+  # attr_accessor :band
+
+  def band=(band)
+    @band = band
+    band.musicians << self
+  end
 
   def initialize(name, instrument)
     @name = name
@@ -29,17 +44,37 @@ class Musician
     instrument.musician = self
   end
 
+
 end #end of class
 
 class Instrument
   attr_accessor :type, :serial_number, :musician
+  @@all = []
 
   def initialize(type, serial_number)
     @type = type
     @serial_number = serial_number
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def band
+    musician.band.name
   end
 
 end #end of class
+
+pirates = Band.new("The Priates")
+tea = Band.new("The Tea Leaves")
+dodgers = Band.new("The Wonky Dodgers")
+stars = Band.new("Star Blazers")
 
 clari = Instrument.new("Clarinet", "AFDAFS")
 trumpt = Instrument.new("Trumpe", "QERQWEUY")
@@ -49,7 +84,9 @@ guitar = Instrument.new("Guitar", "089093845")
 beth = Musician.new("Beth", clari)
 gemma = Musician.new("Gemma", trumpt)
 aki = Musician.new("Aki", piano)
+henry = Musician.new("Henry", guitar)
 
 gemma.band = tea
 beth.band = tea
 aki.band = stars
+henry.band = pirates
